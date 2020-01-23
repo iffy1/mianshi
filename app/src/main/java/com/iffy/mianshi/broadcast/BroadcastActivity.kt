0ccfb9efc12ca92fdcec880e9767e566aebc4903
iffy <zyf8395@sina.com>
2020-01-23 10:51:17 +0800

    localBroadCast

--- app/src/main/java/com/iffy/mianshi/broadcast/BroadcastActivity.kt
+++ app/src/main/java/com/iffy/mianshi/broadcast/BroadcastActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class BroadcastActivity : AppCompatActivity() {
 
@@ -17,6 +18,14 @@ class BroadcastActivity : AppCompatActivity() {
         intentfilter.addAction("woqu")
         this.registerReceiver(receiver, intentfilter)
         this.sendBroadcast(Intent("woqu"))
+       // startActivity(Intent("aa"))
+
+
+        //本地广播防止信息泄露
+        val localIntentfilter = IntentFilter()
+        localIntentfilter.addAction("woqulocal")
+        LocalBroadcastManager.getInstance(this).registerReceiver(MyLocalreceiver(),localIntentfilter)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(Intent("woqulocal"))
         // this.unregisterReceiver(receiver)
     }
 
@@ -24,6 +33,11 @@ class BroadcastActivity : AppCompatActivity() {
         override fun onReceive(p0: Context?, p1: Intent?) {
             println("----我收到消息:${p1!!.action}")
         }
+    }
 
+    inner class MyLocalreceiver : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            println("----我收到消息:${p1!!.action}")
+        }
     }
 }
